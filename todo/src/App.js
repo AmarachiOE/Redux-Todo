e9aee5a60c57c5e02cdef0145950912af3d6b9e6
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-09 13:19:23 -0700

    Moved addNewTodo action creator to onSubmit on form instead of onClick on button on App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,52 +1,53 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 import { addNewTodo, changeInputText, clearCompleted } from "./actions";
 
 import TodoList from "./components/TodoList";
-import './App.css';
+import "./App.css";
 
 // don't need to write this.props.whatever just props.whatever because App is not a class component
 
 const App = props => {
-    return (
-      <div className="App">
+  return (
+    <div className="App">
       <h1>Your Todos</h1>
-      <TodoList/>
-      <input
-            type="string"
-            name="newTask"
-            value={props.newTask}
-            placeholder="What else?"
-            onChange={e => {props.changeInputText(e.target.value)}}
-          />
-      <button
-        onClick={e => {
+      <TodoList />
+      <form
+        onSubmit={e => {
           e.preventDefault();
           props.addNewTodo(props.newTask);
         }}
       >
-      Add Item
-      </button>
+        <input
+          type="string"
+          name="newTask"
+          value={props.newTask}
+          placeholder="What else?"
+          onChange={e => {
+            props.changeInputText(e.target.value);
+          }}
+        />
+        <button>Add Item</button>
+      </form>
       <button
-      onClick={e => {
-        e.preventDefault();
-        props.clearCompleted();
-      }}
+        onClick={e => {
+          e.preventDefault();
+          props.clearCompleted();
+        }}
       >
-      Clear Completed</button>
-      
-      </div>
-    );
-  
-}
+        Clear Completed
+      </button>
+    </div>
+  );
+};
 
 // mSTP gives component access to props
 const mapStateToProps = state => {
   return {
     todos: state.todos,
     newTask: state.newTask
-  }
-}
+  };
+};
 
 export default connect(
   mapStateToProps,
